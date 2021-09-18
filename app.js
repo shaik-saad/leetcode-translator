817b50894d839ea8851bc352a0359e80a3575fda
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-18 21:06:10 +0530

    feat: added eventListener

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 var inputText = document.querySelector("#input-text")
 var btnHack = document.querySelector("#btn-hack")
 var leetCode = document.querySelector("leet-output")
+
+function clickHandler(){
+    console.log("clicked")
+}
+
+btnHack.addEventListener("click", clickHandler)
