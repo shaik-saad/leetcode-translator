b38597cc987a679deb8f546ebb481b52db2cf8c0
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-18 21:23:15 +0530

    feat: added translation functionality

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 // creating references and storing them in variables
-var inputText = document.querySelector("#input-text")
+var inputTextarea = document.querySelector("#input-text")
 var btnHack = document.querySelector("#btn-hack")
-var leetCode = document.querySelector("leet-output")
+var leetCode = document.querySelector("#leet-output")
+
+// server api
+var url = "https://api.funtranslations.com/translate/leetspeak.json"
+
+function getFullURL(value){
+    var fullURL = url + "?text=" + value
+    return fullURL
+}
 
 function clickHandler(){
-    console.log("clicked")
+    //input
+    var inputValue = inputTextarea.value 
+
+    //processing
+    fetch(getFullURL(inputValue))
+    .then(response => response.json())
+    .then(json => {
+        //output
+        leetCode.innerText = json.contents.translated
+    })
 }
 
 btnHack.addEventListener("click", clickHandler)
