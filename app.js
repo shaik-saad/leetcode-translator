895c4c8f8bad4adfadfbbb5829ddabc5ffb7bab1
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-18 20:59:53 +0530

    created references

---
+++ app.js
@@ -0,0 +1,4 @@
+// creating references and storing them in variables
+var inputText = document.querySelector("#input-text")
+var btnHack = document.querySelector("#btn-hack")
+var leetCode = document.querySelector("leet-output")
